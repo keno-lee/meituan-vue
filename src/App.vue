692d0fdd3c84610bb32e0c7b62afe3ac8e38e727
keno.lee <keno.lee@foxmail.com>
2018-05-06 21:11:23 +0800

    项目框架搭建完成

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <mt-header fixed title="美团"></mt-header>
+
+    <!-- 底部 -->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon fa fa-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon fa fa-flash"></span>
+				<span class="mui-tab-label">闪送超市</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon fa fa-shopping-cart"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon fa fa-user-o"></span>
+				<span class="mui-tab-label">我的</span>
+			</a>
+		</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+  header {
+    background-color: #ffd600;
+    color: #000;
+    height: 54px;
+    font-size: 22px;
+  }
+  nav.mui-bar {
+    height: 55px;
+  }
+</style>
+
---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 引入rem布局的适配方案
+// import "@/assets/flexible/adaptor.less"
+// 引入flexible
+// import '../node_modules/lib-flexible/flexible'
+
+// 引入mintUI全部组件
+import Mint from 'mint-ui';
+// 引入mint-ui的css样式
+import "../node_modules/mint-ui/lib/style.min.css"
+Vue.use(Mint);
+// 引入mui的css样式
+import "@/assets/mui/css/mui.min.css"
+// 引入font-awesome
+import "../node_modules/font-awesome/css/font-awesome.min.css"
+
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
